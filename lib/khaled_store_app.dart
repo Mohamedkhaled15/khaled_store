38a40bee4a2b746c08b59f65759aa5c149f85546
mohamedkhaled <mohammedkhaled156212gmail.com>
2024-05-21 16:39:54 +0300

    add assets

--- lib/khaled_store_app.dart
+++ lib/khaled_store_app.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:khaled_store/core/app/env.variables.dart';
+import 'package:khaled_store/core/common/screens/no_network_screen.dart';
 
 class KhaledStoreApp extends StatelessWidget {
   const KhaledStoreApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // return MaterialApp(
+    //   title: 'Khaled Store',
+    //   theme: ThemeData(
+    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    //     useMaterial3: true,
+    //   ),
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: const Text('Khaled Store'),
+    //     ),
+    //   ),
+    //   debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+    // );
+
     return MaterialApp(
-      title: 'Khaled Store',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Khaled Store'),
-        ),
-      ),
+      title: 'No Network',
       debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+      home: const NoNetworkScreen(),
     );
   }
 }
