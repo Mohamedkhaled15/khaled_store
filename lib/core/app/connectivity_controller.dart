0806a19bcb6aff696756bab067ceca73648308eb
mohamedkhaled <mohammedkhaled156212gmail.com>
2024-05-22 11:26:34 +0300

    connectivity plus done

--- lib/core/app/connectivity_controller.dart
+++ lib/core/app/connectivity_controller.dart
@@ -1,30 +1,30 @@
-// import 'package:connectivity_plus/connectivity_plus.dart';
-// import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
 
-// class ConnectivityController {
-//   ConnectivityController._();
+class ConnectivityController {
+  ConnectivityController._();
 
-//   static final ConnectivityController instance = ConnectivityController._();
+  static final ConnectivityController instance = ConnectivityController._();
 
-//   ValueNotifier<bool> isConnected = ValueNotifier(true);
+  ValueNotifier<bool> isConnected = ValueNotifier(true);
 
-//   Future<void> init() async {
-//     final result = await Connectivity().checkConnectivity();
+  Future<void> init() async {
+    final result = await Connectivity().checkConnectivity();
 
-//     isInternetConnected(result);
+    isInternetConnected(result);
 
-//     Connectivity().onConnectivityChanged.listen(isInternetConnected);
-//   }
+    Connectivity().onConnectivityChanged.listen(isInternetConnected);
+  }
 
-//   bool isInternetConnected(ConnectivityResult? result) {
-//     if (result == ConnectivityResult.none) {
-//       isConnected.value = false;
-//       return false;
-//     } else if (result == ConnectivityResult.mobile ||
-//         result == ConnectivityResult.wifi) {
-//       isConnected.value = true;
-//       return true;
-//     }
-//     return false;
-//   }
-// }
+  bool isInternetConnected(ConnectivityResult? result) {
+    if (result == ConnectivityResult.none) {
+      isConnected.value = false;
+      return false;
+    } else if (result == ConnectivityResult.mobile ||
+        result == ConnectivityResult.wifi) {
+      isConnected.value = true;
+      return true;
+    }
+    return false;
+  }
+}
