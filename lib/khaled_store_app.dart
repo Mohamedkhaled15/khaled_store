da151cb09dcd86eb5f09f6b4d9b44bda7c09976f
mohamedkhaled <mohammedkhaled156212gmail.com>
2024-05-22 15:09:24 +0300

    font family & weight done

--- lib/khaled_store_app.dart
+++ lib/khaled_store_app.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:khaled_store/core/app/connectivity_controller.dart';
 import 'package:khaled_store/core/app/env.variables.dart';
 import 'package:khaled_store/core/common/screens/no_network_screen.dart';
+import 'package:khaled_store/core/style/fonts/font_family_helper.dart';
 
 class KhaledStoreApp extends StatelessWidget {
   const KhaledStoreApp({super.key});
@@ -34,6 +35,41 @@ class KhaledStoreApp extends StatelessWidget {
               home: Scaffold(
                 appBar: AppBar(
                   title: const Text('Khaled Store'),
+                ),
+                body: const Column(
+                  children: [
+                    Text(
+                      'Khaled Store',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      'ابو خالد ',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 1,
+                    ),
+                    Text(
+                      'Khaled Store',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: FontFamilyHelper.poppinsEnglish,
+                      ),
+                    ),
+                    Text(
+                      'ابو خالد ',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: FontFamilyHelper.cairoArabic,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 1,
+                    ),
+                  ],
                 ),
               ),
               debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
