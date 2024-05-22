0df23f478ff0be0ae2641231202e64eaaad04ffb
mohamedkhaled <mohammedkhaled156212gmail.com>
2024-05-22 14:37:55 +0300

    splash&screenutile done

--- lib/khaled_store_app.dart
+++ lib/khaled_store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:khaled_store/core/app/connectivity_controller.dart';
 import 'package:khaled_store/core/app/env.variables.dart';
 import 'package:khaled_store/core/common/screens/no_network_screen.dart';
@@ -12,28 +13,31 @@ class KhaledStoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            title: 'Khaled Store',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return widget!;
-                  },
-                ),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(
-                title: const Text('Khaled Store'),
+          return ScreenUtilInit(
+            designSize: const Size(393, 852),
+            child: MaterialApp(
+              title: 'Khaled Store',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
               ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(
+                  title: const Text('Khaled Store'),
+                ),
+              ),
+              debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
             ),
-            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
           );
         } else {
           return MaterialApp(
