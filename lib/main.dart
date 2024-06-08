cb519faa860b2ad25bdb7c5407c6981c1a331bf5
mohamedkhaled <mohammedkhaled156212gmail.com>
2024-06-08 14:28:51 +0300

    setup freezed bloc and shared pref

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:khaled_store/core/app/bloc_observer.dart';
 import 'package:khaled_store/core/app/env.variables.dart';
 import 'package:khaled_store/firebase_options.dart';
 import 'package:khaled_store/khaled_store_app.dart';
@@ -23,6 +25,8 @@ void main() async {
       : await Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         );
+
+  Bloc.observer = AppBlocObserver();
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
